265d4bb18ef65dad55c72f9f38ca57c2afd69e19
Daviddeoliveira <ddeoliveira@gaming.tech>
2024-03-04 12:58:06 +0100

    plein de chosse a fix

--- Assets/Script/Obstacles.cs
+++ Assets/Script/Obstacles.cs
@@ -4,6 +4,34 @@ using UnityEngine;
 
 public class Obstacles : MonoBehaviour
 {
+
+    [SerializeField] private Transform obstacles1;
+    [SerializeField] private Transform obstacles2;
+    [SerializeField] private Transform obstacles3;
+    [SerializeField] private Transform obstacles4;
+    [SerializeField] private float LevelWidth;
+    [SerializeField] private float LevelHeight;
+    [SerializeField] private int obstacleCount;
+    [SerializeField] private float minScale;
+    [SerializeField] private float maxScale;
+    [SerializeField] private float minVerticalScale;
+    [SerializeField] private float maxVerticalScale;
+
+
+    public void Generate()
+    {
+        Vector3 position = new Vector3 (Random.value * LevelWidth,0, Random.value * LevelHeight);
+
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(position, Vector3.down, out hitInfo, 100, LayerMask.GetMask("Ground")))
+        {
+            position = new Vector3(position.x, hitInfo.point.y, hitInfo.point.z);
+        }
+
+        Instantiate<Transform>(obstacles1, position, Quaternion.identity, transform);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
